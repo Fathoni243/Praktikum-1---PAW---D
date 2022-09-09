65a382069f657a4ed49c22e16faa380774ff6af6
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2022-09-09 15:41:13 +0700

    Coba
    Coba

---
+++ AhmadFathoni_215150700111021_PAW-D/coba.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+<body>
+    <h1>Halaman HTML</h1>
+
+    <?php
+    echo "Hello World!";
+    echo "Ahmad Fathoni || 215150700111021";
+    ?>
+    
+</body>
+</body>
+</html>
